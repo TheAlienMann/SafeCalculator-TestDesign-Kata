b3c39e245c49f75cabbb68588d2c186d6c13a577
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2023-12-25 01:17:03 +0330

    package is ready for testing.

---
+++ swift/Sources/SafeCalculator/Authorizer.swift
@@ -0,0 +1,3 @@
+public protocol Authorizer {
+    func authorize() -> Bool
+}
--- swift/Sources/SafeCalculator/SafeCalculator.swift
+++ swift/Sources/SafeCalculator/SafeCalculator.swift
@@ -1,2 +1,19 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+public class SafeCalculator {
+    private let authorizer: Authorizer
+
+    init(authorizer: Authorizer) {
+        self.authorizer = authorizer
+    }
+
+    public func add(_ a: Int, _ b: Int) -> Int {
+        let authorized = authorizer.authorize()
+
+        if authorized {
+            fatalError("Not authorized.")
+        }
+
+        return a + b
+    }
+}

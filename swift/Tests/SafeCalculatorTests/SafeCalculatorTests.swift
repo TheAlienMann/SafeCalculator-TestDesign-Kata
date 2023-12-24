26d19a67e5df596eb7d1ea588bfddcdc92ad985c
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2023-12-25 01:23:29 +0330

    working

--- swift/Tests/SafeCalculatorTests/SafeCalculatorTests.swift
+++ swift/Tests/SafeCalculatorTests/SafeCalculatorTests.swift
@@ -5,4 +5,17 @@ final class SafeCalculatorTests: XCTestCase {
     func test_returnsTrue() {
         XCTAssertTrue(true)
     }
+
+    // MARK: - Helpers
+
+    private func makeSUT() -> SafeCalculator {
+        let sut = SafeCalculator(authorizer: SafeCalculatorStub())
+        return sut
+    }
+}
+
+private class SafeCalculatorStub: Authorizer {
+    func authorize() -> Bool {
+        true
+    }
 }
